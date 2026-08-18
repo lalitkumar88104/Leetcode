f74a7aebcf4309413e0eeb9d59edc14503ae6f42
lalitkumar88104 <lalitkumar.j88104@gmail.com>
2026-08-18 19:46:32 +0530

    Time: 77 ms (27.17%) | Memory: 53.8 MB (5.32%) - LeetSync

---
+++ Array/560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt = 0;
+        int sum = 0;
+        map<int, int> prefixsum;
+        prefixsum[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            cnt += prefixsum[sum - k];
+            prefixsum[sum]++;
+        }
+        return cnt;
+    }
+};
